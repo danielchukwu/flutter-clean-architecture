3ceb8d277aabd9ed14669723d04377e42c4ef176
dc <00danzy@gmail.com>
2023-09-27 13:53:31 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_clean_architecture/config/theme/app_themes.dart';
+import 'package:flutter_clean_architecture/features/daily_news/domain/entities/article.dart';
 import 'package:flutter_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 import 'package:flutter_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:flutter_clean_architecture/features/daily_news/presentation/pages/article_detail/article_detail.dart';
 import 'package:flutter_clean_architecture/features/daily_news/presentation/pages/home/daily_news.dart';
+import 'package:flutter_clean_architecture/features/daily_news/presentation/pages/saved_article/saved_article.dart';
 import 'package:flutter_clean_architecture/injection_container.dart';
 
-void main() {
-  initializeDependencies();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDependencies();
   runApp(const MyApp());
 }
 
@@ -22,8 +26,24 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: theme(),
+        onGenerateRoute: AppRoute.onGenerateRoutes,
         home: const DailyNews(),
       ),
     );
+  }
+}
+
+class AppRoute {
+  static Route onGenerateRoutes(RouteSettings settings) {
+    switch(settings.name) {
+      case '/': 
+        return MaterialPageRoute(builder: (_) => const DailyNews(),);
+      case '/article-details':
+        return MaterialPageRoute(builder: (_) => ArticleDetailsView(article: settings.arguments as ArticleEntity),);
+      case '/saved-article': 
+        return MaterialPageRoute(builder: (_) => const SavedArticles());
+      default:
+        return MaterialPageRoute(builder: (_) => const DailyNews(),);
+    }
   }
 }
