efc217c03dec5be441e91199793ead68f74b2406
dc <00danzy@gmail.com>
2023-09-27 13:53:12 +0100

    first commit

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,7 +1,13 @@
+import 'package:flutter_clean_architecture/features/daily_news/data/data_sources/local/app_database.dart';
 import 'package:flutter_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:flutter_clean_architecture/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:flutter_clean_architecture/features/daily_news/domain/repositories/article_repository.dart';
 import 'package:flutter_clean_architecture/features/daily_news/domain/usecases/get_article.dart';
+import 'package:flutter_clean_architecture/features/daily_news/domain/usecases/get_saved_article.dart';
+import 'package:flutter_clean_architecture/features/daily_news/domain/usecases/remove_local_article.dart';
+import 'package:flutter_clean_architecture/features/daily_news/domain/usecases/save_local_article.dart';
+import 'package:flutter_clean_architecture/features/daily_news/domain/usecases/usecase.dart';
+import 'package:flutter_clean_architecture/features/daily_news/presentation/bloc/article/local/local_article_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 
@@ -10,16 +16,24 @@ import 'features/daily_news/presentation/bloc/article/remote/remote_article_bloc
 final sl = GetIt.instance;
 
 Future<void> initializeDependencies() async {
+  // Database
+  final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+  sl.registerSingleton<AppDatabase>(database);
+  
   // Dio
   sl.registerSingleton<Dio>(Dio());
 
   // Dependencies
   sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
-  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl()));
+  sl.registerSingleton<ArticleRepository>(ArticleRepositoryImpl(sl(), sl()));
 
   // UseCases
   sl.registerSingleton<GetArticlesUseCase>(GetArticlesUseCase(sl()));
+  sl.registerSingleton<GetSavedArticleUseCase>(GetSavedArticleUseCase(sl()));
+  sl.registerSingleton<RemoveArticleUseCase>(RemoveArticleUseCase(sl()));
+  sl.registerSingleton<SaveArticleUseCase>(SaveArticleUseCase(sl()));
 
   // Blocs
   sl.registerFactory<RemoteArticlesBloc>(() => RemoteArticlesBloc(sl()));
+  sl.registerFactory<LocalArticleBloc>(() => LocalArticleBloc(sl(), sl(), sl()));
 } 
